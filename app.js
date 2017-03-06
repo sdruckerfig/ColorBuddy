0090a98517a77974f9bdfe2fd8fd66e35217354e
Steve Drucker <sdrucker@figleaf.com>
2017-03-06 06:46:21 -0500

    deploy

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var alexa = require('alexa-app');
-var PORT = process.env.port || 8080;
+var PORT = process.env.port || 80;
 
 var app = express();
 var alexaApp = new alexa.app("colorbuddy");
 
 alexaApp.express({
 	expressApp: app,
-	checkCert: false
-	// debug: true
+	checkCert: false,
+	debug: true
 })
 
 app.set("view engine", "jade");
